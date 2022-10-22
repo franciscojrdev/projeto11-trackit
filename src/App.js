6ca3d3652b1b162004f85608818222cdbe4fad3a
franciscojrdev <franciscojunior7222@gmail.com>
2022-10-22 14:25:27 -0300

    add files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import GlobalStyle from "./assets/styles/GlobalStyle";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
